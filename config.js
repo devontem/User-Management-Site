2f54c0eda6161d60d02c03cf1f84a41f8d6be4da
Devonte M <devontem@live.com>
2015-07-08 04:43:23 +0000

    updated db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //configuration information
 module.exports = {
     'port': process.env.PORT || 8000,
-    'database': 'mongodb://localhost:27017/test',
+    'database': 'mongodb://admin:admin1@ds047682.mongolab.com:47682/heroku_jnwcdmrc',
     'superSecret': 'coLLecTionOFcharsRS' //for bcrypt
 }
